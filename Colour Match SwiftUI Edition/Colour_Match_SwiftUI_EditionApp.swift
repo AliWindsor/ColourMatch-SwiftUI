b8cf1390f473bd5fa5083ed77220fff9aa474526
AliWindsor <aliwindsor@gmail.com>
2021-06-07 20:13:48 +0100

    Initial Commit

---
+++ Colour Match SwiftUI Edition/Colour_Match_SwiftUI_EditionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Colour_Match_SwiftUI_EditionApp.swift
+//  Colour Match SwiftUI Edition
+//
+//  Created by Alicia Windsor on 07/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct Colour_Match_SwiftUI_EditionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
